69a63460e51da87d7cbe4526b5ec9a89b4904805
santi <santialbo@gmail.com>
2013-03-29 01:18:03 -0400

    Refactored to comply with PEP8 as much as posssible

--- buccaneer.py
+++ buccaneer.py
@@ -1,3 +1,6 @@
+"""
+Buccaneer is a python script that scrapes the results in The Pirate Bay
+"""
 from urllib2 import urlopen
 from BeautifulSoup import BeautifulSoup
 from datetime import datetime, timedelta
@@ -6,49 +9,67 @@ import re
 def enum(**enums):
     return type('Enum', (), enums)
 
-ORDER_BY = enum(NAME = 1, SIZE = 3, UPLOADER = 5, SEEDERS = 7, LEECHERS = 9, TYPE = 13)
+ORDER_BY = enum(NAME = 1,
+                SIZE = 3,
+                UPLOADER = 5,
+                SEEDERS = 7,
+                LEECHERS = 9,
+                TYPE = 13)
 
-def search(searchString, pageNumber = 0, orderBy = ORDER_BY.UPLOADER):
-	url = ''.join(["http://thepiratebay.se/search/",
-		   searchString.replace(" ", "%%20"),
-		   "/" + str(pageNumber) + "/" + str(orderBy)])
-	resp = urlopen(url)
-	html = resp.read()
-	soup = BeautifulSoup(html)
-	table = soup.find("table", {"id": "searchResult"})
-	if table == None:
-		return []
-	else:
-		return _parse_search_result_table(table)
+def search(search_string, page = 0, order_by = ORDER_BY.UPLOADER):
+    """
+    Searches for the given string in The Pirate Bay.
+    Returns a list of dictionaries with the information of each torrent.
+    """
+    url = ''.join(["http://thepiratebay.se/search/",
+           search_string.replace(" ", "%%20"),
+           "/" + str(page) + "/" + str(order_by)])
+    resp = urlopen(url)
+    html = resp.read()
+    soup = BeautifulSoup(html)
+    table = soup.find("table", {"id": "searchResult"})
+    if table == None:
+        return []
+    else:
+        return _parse_search_result_table(table)
 
 def _parse_search_result_table(table):
-	trs = table.findAll("tr")
-	del trs[:2]
-	results = []
-	for tr in trs:
-		results.append(_parse_search_result_table_row(tr))
-	return results
+    trs = table.findAll("tr")
+    del trs[:2]
+    results = []
+    for tr in trs:
+        results.append(_parse_search_result_table_row(tr))
+    return results
 
 def _parse_search_result_table_row(tr):
-	res = {}
-	tds = tr.findAll("td")
-	linkName = tds[1].find("a", {"class": "detLink"})
-	res['name'] = linkName.contents[0]
-	res['link'] = linkName["href"]
-	descString = tds[1].find("font").contents[0].replace("&nbsp;", " ")
-	m = re.search("^Uploaded (Today|Y-day|\d\d-\d\d) (\d{4}|\d\d:\d\d), Size (\d+.\d* (?:[KMG]iB))", descString)
-	res['size'] = m.group(3)
-	now = datetime.today()
-	if re.match("\d{4}", m.group(2)) == None:
-		if m.group(1) == "Today":
-			res['time'] = datetime.strptime(now.strftime("%m-%d-%Y") + " " + m.group(2), "%m-%d-%Y %H:%M")
-		elif m.group(1) == "Y-day":
-			res['time'] = datetime.strptime((now + timedelta(-1)).strftime("%m-%d-%Y") + " " + m.group(2), "%m-%d-%Y %H:%M")
-		else:
-			res['time'] = datetime.strptime(m.group(1) + "-" + str(now.year) + " " + m.group(2), "%m-%d-%Y %H:%M")
-	else:
-		res['time'] = datetime.strptime(m.group(1) + "-" + m.group(2), "%m-%d-%Y")
-	res['seeders'] = int(tds[2].contents[0])
-	res['leechers'] = int(tds[3].contents[0])
-	res['magnet'] = tds[1].find("img", {"alt": "Magnet link"}).parent['href']
-	return res
+    res = {}
+    tds = tr.findAll("td")
+    link_name = tds[1].find("a", {"class": "detLink"})
+    res['name'] = link_name.contents[0]
+    res['link'] = link_name["href"]
+    desc_string = tds[1].find("font").contents[0].replace("&nbsp;", " ")
+    m = re.search(r"^Uploaded (Today|Y-day|\d\d-\d\d) (\d{4}|\d\d:\d\d), " +
+                  r"Size (\d+.\d* (?:[KMG]iB))", desc_string)
+    res['size'] = m.group(3)
+    now = datetime.today()
+    if re.match(r"\d{4}", m.group(2)) == None:
+        hour =" " + m.group(2)
+        if m.group(1) == "Today":
+            res['time'] = datetime.strptime(
+                    now.strftime("%m-%d-%Y") + hour,
+                    "%m-%d-%Y %H:%M")
+        elif m.group(1) == "Y-day":
+            res['time'] = datetime.strptime(
+                    (now + timedelta(-1)).strftime("%m-%d-%Y") + hour,
+                     "%m-%d-%Y %H:%M")
+        else:
+            res['time'] = datetime.strptime(
+                    m.group(1) + "-" + str(now.year) + hour,
+                    "%m-%d-%Y %H:%M")
+    else:
+        res['time'] = datetime.strptime(m.group(1) + "-" + m.group(2),
+                "%m-%d-%Y")
+    res['seeders'] = int(tds[2].contents[0])
+    res['leechers'] = int(tds[3].contents[0])
+    res['magnet'] = tds[1].find("img", {"alt": "Magnet link"}).parent['href']
+    return res
--- example.py
+++ example.py
@@ -1,5 +1,5 @@
 import buccaneer
 
-results = buccaneer.search('Weeds s08e04 720p', 0, buccaneer.order_by.SEEDERS)
+results = buccaneer.search('Weeds s08e04 720p', 0, buccaneer.ORDER_BY.SEEDERS)
 for r in results:
 	print '{0} {1:5d}   {2}'.format(r['name'].ljust(45), r['seeders'], r['magnet'])
